043b40ac3aacf2730b6924900ea008ddcf9c0f94
mans-persson-nti-johanneberg <mans.persson@elev.ga.ntig.se>
2024-02-01 16:09:49 +0100

    greher

--- src/app.rb
+++ src/app.rb
@@ -1,5 +1,8 @@
 class App < Sinatra::Base
 
+
+    @loggedin = 1
+
     def db
         if @db == nil
             @db = SQLite3::Database.new('./db/db.sqlite')
@@ -9,7 +12,16 @@ class App < Sinatra::Base
     end
 
     get '/' do
+        @people = db.execute('SELECT firstname, lastname, id FROM users')
+        p @people[0]['firstname']
         erb :index
+    end
+
+    get '/register' do
+        
+
+
+        erb :register
     end
 
     
